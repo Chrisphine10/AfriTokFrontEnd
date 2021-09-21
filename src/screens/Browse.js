154d30a1f5645710476808be0635f9ea6d5e4288
Chrisphine10 <ondiekchrisphine@gmail.com>
2021-09-21 03:13:27 +0300

    browse module

--- src/screens/Browse.js
+++ src/screens/Browse.js
@@ -1,20 +1,56 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, Dimensions, SafeAreaView, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, FlatList, Text, Dimensions, SafeAreaView, TouchableOpacity} from 'react-native';
 import styles from '../styles/browsestyles';
 import { Searchbar } from 'react-native-paper';
-import Carousel from 'react-native-snap-carousel';
+import MasonryList from '@react-native-seoul/masonry-list';
+import test from '../../test.json';
 
 const Browse = () => {
-    const carousel = useRef(null);
-    const {width, height} = Dimensions.get('window');
-    const renderItem = () => (
-        <View>
-            <Text>test</Text>
-        </View>
-    );
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const API_KEY = "563492ad6f91700001000001562a8e890c124dc78e84162aaa8f119c"
+    const API_URL = "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20"
+
+    const getImages = async () => {
+        try {
+          const response = await fetch(API_URL,
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${API_KEY}`
+                    }
+                });
+          const { photos } = await response.json();
+          return photos;
+        } catch (error) {
+          console.error(error);
+        }
+    };
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+                const images = await getImages();
+                setData(images);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
+    if(isLoading) {
+        return <View><Text>Loading...</Text></View>;
+    }
     return (
         <SafeAreaView style={styles.AndroidSafeArea}>
-            <View style={styles.search}>
+            <TouchableOpacity
+            onPress={() => {
+                console.warn('Search Pressed')
+            }}
+            style={styles.search}>
                 <Searchbar
                     style={{
                         borderRadius: 30,
@@ -24,16 +60,35 @@ const Browse = () => {
                         fontSize: 14,
                         fontFamily: 'AbelRegular',
                     }}
-                    disable
+                    selectTextOnFocus={false}
                     placeholder="Search"
+                    editable={false}
                 />
-            </View>
+            </TouchableOpacity>
             <View>
-                <Carousel
-                ref={carousel}
-                renderItem={renderItem}
-                sliderWidth={width}
-                itemWidth={width - 10}
+                <MasonryList
+                    data={data}
+                    keyExtractor={item=>item.id.toString()}
+                    contentContainerStyle={ {
+                        margin: 0,
+                        backgroundColor: '#fff',
+                    }}
+                    renderItem={({ item }) => (
+                        <View>
+                            <Image
+                                source={{ uri: item.src.portrait }}
+                                style={{
+                                    margin: 0.5,
+                                    width: Dimensions.get('window').width/3.05, 
+                                    height: Dimensions.get('window').width * 0.75,
+                                    borderRadius: 5,
+                                    resizeMode: 'cover',
+
+                                }}
+                                />
+                        </View>
+                    )}
+                    numColumns={3}
                 />
             </View>
         </SafeAreaView>
